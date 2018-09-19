d51e228e4e63c67b5d0e70b34e58e0c1f4cdbf36
Ian M <ian.hg.dev@gmail.com>
2018-09-19 19:01:52 +0900

    Working token comparison

--- __main__.py
+++ __main__.py
@@ -5,13 +5,17 @@ import logging
 from NichieiComparer import ExcelDoc
 
 def main():
-    """ Main body for starting up and terminating Tweetfeeder bot """
+    """ Main body for starting up and terminating program """
     # pylint: disable=no-member
     os.environ['MECAB_CHARSET'] = 'UTF-8'
     logging.basicConfig(level=logging.DEBUG)
     try:
         translated_doc = ExcelDoc('./Samples/subtitle_translation.xlsx', translated=True)
         untranslated_doc = ExcelDoc('./Samples/subtitle_untranslated.xlsx', translated=False)
+        term_overlap = untranslated_doc.map_matching_tokens(translated_doc)
+        for key, value in term_overlap.items():
+            token = untranslated_doc.tokens[key].token + ":" + translated_doc.tokens[value[0]].token
+            print(f"{key}:\t{value} --\t{token}")
     except KeyboardInterrupt:
         print("Forced exit")
 
